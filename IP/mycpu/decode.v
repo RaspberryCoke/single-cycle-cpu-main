0802c3038b66a9627a12f5829a5c2ca4108a09c3
ydliu <3205516076@qq.com>
2025-02-14 21:02:31 +0800

    完成decode

--- IP/mycpu/decode.v
+++ IP/mycpu/decode.v
@@ -1,8 +1,24 @@
-module decode(
-    input wire clk,
-    input wire rst,
-    input wire[31:0] instr
-);
-
-
+module decode(input wire clk,
+              input wire rst,
+              input wire[31:0] instr,
+              input wire[4:0] Ra,
+              input wire[4:0] Rb,
+              input wire[4:0] Rw,
+              input wire RegWr,
+              input wire[31:0] busW,
+              output wire[31:0]busA,
+              output wire[31:0]busB);
+    
+    regfile rf(
+    .clk(clk),
+    .rst(rst),
+    .rs1_id_i(Ra),
+    .rs2_id_i(Rb),
+    .rs1_rdata_o(busA),
+    .rs2_rdata_o(busB),
+    .w_en(RegWr),
+    .rd_id_i(Rw),
+    .rd_write_data_i(busW)
+    );
+    
 endmodule
--- IP/mycpu/execute.v
+++ IP/mycpu/execute.v
@@ -1,9 +1,23 @@
 module execute(
     input wire clk,
     input wire rst,
-    input wire[31:0] instr
+    input wire[31:0] instr,
+    input wire[31:0] pc,
+    input wire[31:0] busA,
+    input wire[31:0] busB,
+    input wire[31:0] imm,
+    input wire ALUAsrc,
+    input wire[1:0] ALUBsrc,
+    input wire[3:0] ALUctr,
+    input wire[2:0] Branch,
+    output wire Less,
+    output wire Zero,
+    output wire[31:0] Result,
+    output wire[31:0] NextPC
 );
 
+    wire PCAsrc;
+    wire PCBsrc;
 
 endmodule
 
